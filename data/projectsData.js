2145e6d9dd9cdfc7aa773151880d1f3dc714e7c4
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2022-01-22 16:01:45 -0500

    updated portfolio

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,17 +1,5 @@
 const projectsData = [
   {
-    title: 'Course Timetable Screenshot to Excel Sheet',
-    description: `Converts an image of a college course timetable into an excel sheet.`,
-    imgSrc: '/static/images/image-to-excel.png',
-    repoHref: 'https://github.com/semajthomasprimm/image-to-excel',
-  },
-  {
-    title: 'Meeting Scheduler Slack Bot',
-    description: `Schedule Google events from Slack with commands. Integrated with Stripe for billing.`,
-    imgSrc: '/static/images/slack-scheduler-bot.png',
-    repoHref: 'https://github.com/semajthomasprimm/meeting-scheduler-slack-bot',
-  },
-  {
     title: 'Cryptocurrency Dashboard',
     description: `A price tracker for top 100 cryptocurrencies based on CoinGecko.`,
     imgSrc: '/static/images/crypto-dashboard.png',
@@ -25,13 +13,13 @@ const projectsData = [
     href: 'https://covid19-canada-dashboard.netlify.app/',
     repoHref: 'https://github.com/semajthomasprimm/covid19-canada-dashboard',
   },
-  {
+  /* {
     title: 'WardTo',
     description: `Competed in Hack the North with a team of 4 to build a web app in 36 hours, dedicated to helping people in need find shelters in Toronto.`,
     imgSrc: '/static/images/ward-to.png',
     href: 'https://wardto.tech/',
     repoHref: 'https://github.com/semajthomasprimm/hackthenorth',
-  },
+  }, */
 ]
 
 export default projectsData
